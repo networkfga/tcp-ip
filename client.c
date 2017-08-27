26e1dcbc9071201f876107017b05f4307bbe352e
Pedro Ivo de Andrade <andradepedroivo@gmail.com>
2017-08-26 21:58:29 -0300

    Add Client module

--- client.c
+++ client.c
@@ -6,11 +6,17 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+int connect_to_socket();
+
 char serverIp[] = "192.168.0.11";
 int socketId = 0;
 struct sockaddr_in serverAddress;
 int bufferLength = 0;
 unsigned short serverPort = 9600;
+
+void main (void){
+    int result = connect_to_socket();
+}
 
 int connect_to_socket(void){
     socketId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
