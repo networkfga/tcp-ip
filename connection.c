2804c1e20caf35a602fe889dbc17fddae9507b30
Pedro Ivo de Andrade <andradepedroivo@gmail.com>
2017-08-26 21:02:41 -0300

    Add connection to socket
    Socket connection necessary to server/client architecture.
    
    Signed-off-by: Pedro Ivo de Andrade <andradepedroivo@gmail.com>

---
+++ connection.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+char serverIp[] = "192.168.0.11";
+int socketId = 0;
+struct sockaddr_in serverAddress;
+int bufferLength = 0;
+unsigned short serverPort = 9600;
+
+int connect_to_socket(void){
+    socketId = socket(AF_INET, SOCK_STREAM, IPPROT_TCP);
+    if (socketId < 0){
+        fprintf(stderr, "Erro na criacao do socket!\n");
+        exit(0);
+    }
+
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(serverIp);
+    serverAddress.sin_port = htons(serverPort);
+
+    if (connect(socketId, (struct sockaddr *) &serverAddress, 
+                sizeof(serverAddress)) < 0){
+        fprintf(stderr, "Erro de Conexao.\n");
+        exit(0);
+    }
+
+    return socketId;
+}
